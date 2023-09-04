061a2fa8950ec53fb0af9b6aac40036174025bb8
SilverGamer <1795959147@qq.com>
2023-09-04 00:19:29 -0400

    #0 the initialization of Glimmer

---
+++ apps/main.cpp
@@ -0,0 +1,24 @@
+
+
+#include "headers.h"
+
+
+const uint8_t g_famesNum = 3;
+bool g_useWarp = false;
+
+int g_clientWidth = 1280;
+int g_clientHeight = 720;
+
+bool g_isInitialized = false;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ include/headers.h
@@ -0,0 +1,45 @@
+
+#ifndef SG_HEADERS
+#define SG_HEADERS
+
+#include <Windows.h>
+#include <shellapi.h>
+
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+#include <wrl.h>
+
+using namespace Microsoft::WRL;
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include "d3dx12.h"
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
+inline void ThrowIfFailed(HRESULT hr)
+{
+	if (FAILED(hr)) {
+		throw std::exception();
+	}
+}
+
+
+#endif
+
+
